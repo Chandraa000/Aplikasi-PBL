3a56abf4f4f7bbac66f99d7201d571b1aee55106
Chandra.github.io <biskuatcoklat157@gmail.com>
2026-05-06 17:55:44 +0800

    update GroupMember

--- app/Models/GroupMember.php
+++ app/Models/GroupMember.php
@@ -22,4 +22,9 @@ class GroupMember extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function project()
+    {
+    return $this->belongsTo(Project::class);
+    }
 }
